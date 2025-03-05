b4d3c0e98708a829e81bce4117c911815bbb90d6
CangNguyen <brunomessi933@gmail.com>
2025-03-05 14:47:08 +0700

    update setup

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -12,6 +13,11 @@ async function bootstrap() {
     .setVersion('1.0')
     .addBearerAuth() // Enable JWT Authentication (if needed)
     .build();
+
+  // Enable validation globally
+  app.useGlobalPipes(
+    new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }),
+  );
 
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api/docs', app, document);
